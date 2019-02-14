4cbd5c139454cf6b11e8d0fc41709120ae6fe386
kencharos <many.front@gmail.com>
2019-02-14 15:35:50 +0900

    add round robin

--- grpcfront/src/main/java/my/grpcfront/ChannelManager.java
+++ grpcfront/src/main/java/my/grpcfront/ChannelManager.java
@@ -10,8 +10,10 @@ import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.EurekaClientConfig;
 
+import io.grpc.LoadBalancerRegistry;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.util.RoundRobinLoadBalancerFactory;
 import my.eureka.EurekaNameResolverProvider;
 
 @Component
@@ -34,9 +36,10 @@ public class ChannelManager {
 
     private ManagedChannel createChannel() {
 
-
         final ManagedChannel channel = ManagedChannelBuilder.forTarget("eureka://" + "grpc")
                                                             .nameResolverFactory(new EurekaNameResolverProvider(config))
+                                                            // use all service round robin in single channel
+                                                            .defaultLoadBalancingPolicy("round_robin")
                                                             .usePlaintext()
                                                             .build();
         return channel;
